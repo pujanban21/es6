8f02f8c2a50e25552896deb483c778bd406fa89f
Pujan <pujan_567@hotmail.com>
2019-03-26 10:26:32 -0400

    practice 1

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
 const hellWord = () => {
 	console.log("Arrow function works!");
+	console.log(a);
+
+	let a = () => 45;
+	console.log(typeof a, a());
+}
+
+function getContext() {
+	console.log(this);
+}
+
+let myObj = {
+	name: 'fancy',
+	operation: function () {
+		console.log(this);
+	}
 }
 hellWord();
+getContext();
+myObj.operation();
